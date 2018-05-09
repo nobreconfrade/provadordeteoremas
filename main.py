316fa21ed57029c015486578b81ff90471cbe839
Your Name <willpereirabr@gmail.com>
2018-05-08 22:05:33 -0300

    olha essa func de arvore

--- main.py
+++ main.py
@@ -6,7 +6,39 @@ import Formula
 
 count = 0
 cardinality = 0
+rawGlobal = []
+treeList = []
+treeReadyList = []
 formList = []
+
+def createTree():
+    if rawGlobal[0] == '*' or rawGlobal[0] == '+' or rawGlobal[0] == ')':
+        formula = Formula.Formula
+        formula.str = rawGlobal[0]
+        rawGlobal.pop(0)
+        formula.left = createTree()
+        rawGlobal.pop(0)
+        formula.right = createTree()
+        treeList.insert(0,formula)
+    elif rawGlobal[0] == '-':
+        formula = Formula.Formula
+        formula.str = rawGlobal[0]
+        rawGlobal.pop(0)
+        formula.left = createTree()
+        formula.right = None
+        treeList.insert(0,formula)
+    elif rawGlobal[0].isdigit():
+        formula = Formula.Formula
+        formula.str == rawGlobal[0]
+        rawGlobal.pop(0)
+        formula.left = None
+        formula.right = None
+        treeList.insert(0,formula)
+    else:
+        '''
+            O QUE????
+        '''
+    return formula
 
 with open('test/test1.seq') as f:
     for l in f:
@@ -17,10 +49,9 @@ with open('test/test1.seq') as f:
             raw = l.split(" ")
             raw[len(raw)-1] = raw[len(raw)-1].rstrip()
             # raw.insert(0,'1')
-            formula.l = raw
-            formula.beta = 0
-            formula.valor = True
-            formList.append(formula)
+            formList.append(raw)
         count += 1
-    formList[len(formList)-1].valor = False
-    print(formList)
+for i in formList:
+    rawGlobal = i
+    treeList = []
+    treeReadyList.append(createTree())
