36239d86f71f68b7040523d4be9bbcedd9d18e0a
Kronossaurus <brvictor.chess@hotmail.com>
2018-05-08 22:25:31 -0300

    Python3 + stuff

--- main.py
+++ main.py
@@ -7,33 +7,21 @@ import Formula
 count = 0
 cardinality = 0
 rawGlobal = []
-treeList = []
-treeReadyList = []
 formList = []
 
 def createTree():
-    if rawGlobal[0] == '*' or rawGlobal[0] == '+' or rawGlobal[0] == ')':
-        formula = Formula.Formula
-        formula.str = rawGlobal[0]
-        rawGlobal.pop(0)
+    formula = Formula.Formula
+    formula.str = rawGlobal[0]
+    rawGlobal.pop(0)
+    if (rawGlobal[0] in ['*','+',')']):        
         formula.left = createTree()
-        rawGlobal.pop(0)
         formula.right = createTree()
-        treeList.insert(0,formula)
-    elif rawGlobal[0] == '-':
-        formula = Formula.Formula
-        formula.str = rawGlobal[0]
-        rawGlobal.pop(0)
+    elif (rawGlobal[0] == '-'):
         formula.left = createTree()
         formula.right = None
-        treeList.insert(0,formula)
-    elif rawGlobal[0].isdigit():
-        formula = Formula.Formula
-        formula.str == rawGlobal[0]
-        rawGlobal.pop(0)
+    elif (rawGlobal[0].isdigit()):
         formula.left = None
         formula.right = None
-        treeList.insert(0,formula)
     else:
         '''
             O QUE????
@@ -42,7 +30,7 @@ def createTree():
 
 with open('test/test1.seq') as f:
     for l in f:
-        if count == 0:
+        if (count == 0):
             cardinality = l
         else:
             formula = Formula.Formula
@@ -51,7 +39,5 @@ with open('test/test1.seq') as f:
             # raw.insert(0,'1')
             formList.append(raw)
         count += 1
-for i in formList:
-    rawGlobal = i
-    treeList = []
-    treeReadyList.append(createTree())
+        rawGlobal = l
+        formList.append(createTree)
